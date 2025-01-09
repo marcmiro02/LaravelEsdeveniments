f2ea29c4fe1a50948827319cd78efc53146469af
Alemusicaria <aleixpratmarin@gmail.com>
2025-01-09 09:45:42 +0100

    ASDASD

--- laravelEsdeveniments/routes/web.php
+++ laravelEsdeveniments/routes/web.php
@@ -8,12 +8,71 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+//------------------------------------- DASHBOARD -------------------------------------//
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+//--------------------------------------- USERS ---------------------------------------//
+//Route::resource('users', UsersController::class)->parameters([
+//    'users' => 'id'
+//])->middleware(['auth', 'verified']);
+
+
+//-------------------------------------- EMPRESES --------------------------------------//
 Route::resource('empreses', EmpresesController::class)->parameters([
     'empreses' => 'id_empresa'
 ])->middleware(['auth', 'verified']);
+
+
+//----------------------------------- ESDEVENIMENTS -----------------------------------//
+//Route::resource('esdeveniments', EsdevenimentsController::class)->parameters([
+//    'esdeveniments' => 'id_esdeveniment'
+//])->middleware(['auth', 'verified']);
+
+
+//------------------------------------- CATEGORIES -------------------------------------//
+//Route::resource('categories', CategoriesController::class)->parameters([
+//    'categories' => 'id_categoria'
+//])->middleware(['auth', 'verified']);
+
+
+//------------------------------------ ESTAT_SEIENTS ------------------------------------//
+//Route::resource('estat_seients', EstatSeientsController::class)->parameters([
+//    'estat_seients' => 'id_estat_seient'
+//])->middleware(['auth', 'verified']);
+
+
+//-------------------------------------- SEIENTS --------------------------------------//
+//Route::resource('seients', SeientsController::class)->parameters([
+//    'seients' => 'id_seient'
+//])->middleware(['auth', 'verified']);
+
+
+//---------------------------------------- qr ----------------------------------------//
+//Route::resource('qr', QrController::class)->parameters([
+//    'qr' => 'id_qr'
+//])->middleware(['auth', 'verified']);
+
+
+//------------------------------------ ROLS_USUARIS ------------------------------------//
+//Route::resource('rols_usuaris', RolsUsuarisController::class)->parameters([
+//    'rols_usuaris' => 'id_rol'
+//])->middleware(['auth', 'verified']);
+
+
+//------------------------------------ SALES ------------------------------------//
+//Route::resource('sales', SalesController::class)->parameters([
+//    'sales' => 'id_sala'
+//])->middleware(['auth', 'verified']);
+
+
+//------------------------------------ TIPUS_ESDEVENIMENTS ------------------------------------//
+//Route::resource('tipus_esdeveniments', TipusEsdevenimentsController::class)->parameters([
+//    'tipus_esdeveniments' => 'id_tipus'
+//])->middleware(['auth', 'verified']);
 
 
 Route::middleware('auth')->group(function () {
