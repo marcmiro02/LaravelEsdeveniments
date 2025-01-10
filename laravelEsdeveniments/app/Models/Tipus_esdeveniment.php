6d0ad89bf05b8ace7770c0430e342bf6b966b7a6
Alemusicaria <aleixpratmarin@gmail.com>
2025-01-10 09:38:50 +0100

    sdfsdf

--- laravelEsdeveniments/app/Models/Tipus_esdeveniment.php
+++ laravelEsdeveniments/app/Models/Tipus_esdeveniment.php
@@ -7,48 +7,49 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tipus_esdeveniment extends Model
 {
-        use HasFactory;
-        /**
-         * The table associated with the model.
-         *
-         * @var string
-         */
-        protected $table = 'tipus_esdeveniment';
-    
-        /**
-         * The attributes that are mass assignable.
-         *
-         * @var array<int, string>
-         */
-        protected $fillable = [
-            'nom',
-        ];
-    
-        /**
-         * The primary key associated with the table.
-         *
-         * @var string
-         */
-        protected $primaryKey = 'id_tipus';
-    
-        /**
-         * Indicates if the IDs are auto-incrementing.
-         *
-         * @var bool
-         */
-        public $incrementing = true;
-    
-        /**
-         * The data type of the auto-incrementing ID.
-         *
-         * @var string
-         */
-        protected $keyType = 'int';
-    
-        /**
-         * Indicates if the model should be timestamped.
-         *
-         * @var bool
-         */
-        public $timestamps = false;
-    }
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tipus_esdeveniments';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nom_tipus',
+    ];
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id_tipus';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'int';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+}
