cab6e6e4cc0cd6d48c178043e77fb3891a10977f
Alemusicaria <aleixpratmarin@gmail.com>
2025-01-13 10:20:21 +0100

    asdasd

--- laravelEsdeveniments/resources/views/layouts/navigation.blade.php
+++ laravelEsdeveniments/resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <img src="" alt=""> 
+                        <img src="{{ asset('img/Logos/Fosc_sense_bg.png') }}" alt="" class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200">  
                     </a>
                 </div>
 
