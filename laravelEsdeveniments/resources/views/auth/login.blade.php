81d32a7110b3e88cc248a30dd64a6fc87a2c5777
Alemusicaria <aleixpratmarin@gmail.com>
2025-02-27 01:07:46 +0100

    asdasd

--- laravelEsdeveniments/resources/views/auth/login.blade.php
+++ laravelEsdeveniments/resources/views/auth/login.blade.php
@@ -16,14 +16,14 @@
 
                 <!-- Email Address -->
                 <div>
-                    <x-input-label for="email" :value="__('Email')" />
+                    <x-input-label for="email" :value="__('Correu')" />
                     <x-text-input id="email" class="block mt-1 w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-lg focus:outline-none focus:border-rose-600 text-white" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                 </div>
 
                 <!-- Password -->
                 <div>
-                    <x-input-label for="password" :value="__('Password')" />
+                    <x-input-label for="password" :value="__('Contrasenya')" />
                     <x-text-input id="password" class="block mt-1 w-full px-4 py-3 bg-gray-800 border border-gray-700 rounded-lg focus:outline-none focus:border-rose-600 text-white" type="password" name="password" required autocomplete="current-password" />
                     <x-input-error :messages="$errors->get('password')" class="mt-2" />
                 </div>
@@ -32,7 +32,7 @@
                 <div class="block mt-4">
                     <label for="remember_me" class="inline-flex items-center">
                         <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                        <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                        <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Recorda\'m') }}</span>
                     </label>
                 </div>
 
